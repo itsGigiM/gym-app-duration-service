0a5a6695748b917f943b0a7d02052349f527fbed
itsGigiM <113678374+itsGigiM@users.noreply.github.com>
2024-05-22 11:08:18 +0400

    initial commit

---
+++ src/main/java/com/example/totalDuration/TotalDurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.totalDuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalDurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotalDurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/totalDuration/TotalDurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.totalDuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalDurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
